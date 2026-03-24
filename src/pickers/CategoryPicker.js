386d77ecf6f7f6e579efc59ff4ebb50be518875d
Anthony BELEM <anthbel@gmail.com>
2026-03-24 07:33:46 +0200

    refactor: code refactoring

--- src/pickers/CategoryPicker.js
+++ src/pickers/CategoryPicker.js
@@ -27,33 +27,39 @@ function CategoryPicker(props) {
   const { formatMessage } = useTranslations('ticket');
   const [inputValue, setInputValue] = useState(value ?? '');
 
+  React.useEffect(() => {
+    setInputValue(value ?? '');
+  }, [value]);
+
   const { data, error } = useGraphqlQuery(
     `query CategoryPicker {
         grievanceConfig{
           grievanceCategoriesHierarchical{
-            name fullName priority permissions defaultFlags 
+            name fullName
             children {
-              name fullName priority permissions defaultFlags
+              name fullName
             }
           }
         }
     }`,
+    {},
     { skip: true },
   );
 
   const options = data?.grievanceConfig?.grievanceCategoriesHierarchical.map((type) => ({
     label: type.name,
     value: type.fullName,
-    children: type.children.map((child) => ({
+    children: (type.children ?? []).map((child) => ({
       label: child.name,
       value: child.fullName,
-    })) ?? [],
+    })),
   })) ?? [];
 
   const handleCascaderChange = (values, selectedOptions) => {
-    const selected = selectedOptions.pop();
-    setInputValue(selected?.value || '');
-    onChange?.(selected.value);
+    const selected = selectedOptions && selectedOptions[selectedOptions.length - 1];
+    const stringValue = selected?.value ?? null;
+    setInputValue(stringValue || '');
+    onChange?.(selected, stringValue);
   };
 
   const defaultValue = () => {
@@ -68,7 +74,7 @@ function CategoryPicker(props) {
   return (
     <div className={classes.root}>
       <Cascader
-        defaultValue={defaultValue()}
+        value={defaultValue()}
         multiple={multiple}
         options={options}
         onChange={handleCascaderChange}
@@ -85,7 +91,7 @@ function CategoryPicker(props) {
           value={inputValue}
           fullWidth
           disabled={readOnly}
-          error={error}
+          error={!!error}
           InputProps={{
             readOnly: true,
             endAdornment: (<ArrowDropDownIcon
@@ -98,4 +104,4 @@ function CategoryPicker(props) {
   );
 }
 
-export default withStyles(styles)(withTheme(CategoryPicker));
+export default withTheme(withStyles(styles)(CategoryPicker));
